544666afd32f542a2bd71118f35fd490ed6c81f8
Artolaf <56573835+Artolaf@users.noreply.github.com>
2023-12-03 18:25:08 +0100

    added first simple check for blacklisted words

--- Start.py
+++ Start.py
@@ -2,10 +2,12 @@ from vosk import Model, KaldiRecognizer
 import pyaudio
 
 
-def STT():
-    model = Model(r"C:\Gebaerdenstuff\vosk\vosk-model-de-0.21") 
+def speach_to_text():
+    print('start.py - start loading model')
+    #model = Model(r"D:\Dokumente\Schule\Klasse 10\Franzosisch\forschung\vosk-model-de-0.21")
+    model = Model(r"D:\Dokumente\Schule\Klasse 10\Franzosisch\forschung\vosk-model-small-de-zamia-0.3")
     recog = KaldiRecognizer(model, 16000)
-    print("done")
+    print("start.py - loaded model successfully")
 
     cap = pyaudio.PyAudio()
     stream = cap.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8192)
@@ -17,6 +19,6 @@ def STT():
             break
         if recog.AcceptWaveform(data):
             
-            return(recog.Result().split(('"')))
+            return(recog.Result().split(('"'))[3])
 
 
--- video-test.py
+++ video-test.py
@@ -1,21 +1,29 @@
 from moviepy.editor import VideoFileClip, concatenate_videoclips
 import os
-import Start
+from Start import speach_to_text
 
-path = r"C:\Gebaerdenstuff\dgs_woerterbuch_gesamt\Gebaerden\video" #path to the Gebaerdensprachen video files
+path = r"D:\Dokumente\Schule\Klasse 10\Franzosisch\forschung\dgs_vids\video" #path to the Gebaerdensprachen video files
 
-
-input = Start.STT()
-print("das ist der input:", input)
-word_list_raw = input[3]
-word_list = word_list_raw.split()
+"""
+input:str = speach_to_text()
+word_list = input.split()
+print(f'speach_to_text word_list: {word_list}')
 
 
 
 """
-word_list_raw = "auto bombe"
+word_list_raw = "auto und reifen"
 word_list = word_list_raw.split()
-"""
+
+
+
+#first check for simple blacklisted words
+with open('blacklist_words_dgs.txt') as f:
+    blacklist = f.read().split('\n')
+    for word in word_list:
+        if(word in blacklist):
+            word_list.remove(word)
+            print(word)
 
 
 clip_list = []
@@ -25,26 +33,20 @@ all_clips_found = True
 
 for word in word_list:                                  #Load clips into memory
     if word in saved_clips:
-
-        clip = clip_list[word]
+        clip = clip_list[clip_list.index[word]]
         
     else:
         try:
             clip = VideoFileClip(os.path.join(path, word + ".mp4"))
             saved_clips[word] = clip
-        except:
-            all_clips_found = False
+
+        except RuntimeError as e:
+            print(e)
+            raise RuntimeError(f'One of the words did not exist: {word}')
         
 
     clip_list.append(clip)                                      
 
 
-
-if all_clips_found == False:
-    print("One of the words didn't exist")
-else:
-    final_clip = concatenate_videoclips(clip_list)
-    final_clip.write_videofile("my_concatenation.mp4")
-
-
-
+final_clip = concatenate_videoclips(clip_list)
+final_clip.write_videofile("output.mp4")
